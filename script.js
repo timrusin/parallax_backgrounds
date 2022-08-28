7637b4d60e39c6b86682b773768561590353b5e5
timrusin <timrusin@gmail.com>
2022-08-27 23:42:56 -0500

    parallax background with object oreinted programing complete

---
+++ script.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 1;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = './images/layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = './images/layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = './images/layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = './images/layer-4.png';
+const backgroundLayer5 = new Image()
+backgroundLayer5.src = './images/layer-5.png';
+
+class Layer {
+    constructor(image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+            if (this.x <= -this.width){
+                this.x = this.width + this.x2 - this.speed;
+            }
+            if (this.x2 <= -this.width){
+                this.x2 = this.width + this.x - this.speed;
+            }
+            this.x = Math.floor(this.x - this.speed);
+            this.x2 = Math.floor(this.x2 -this.speed)
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const Layer1 = new Layer(backgroundLayer1, .25);
+const Layer2 = new Layer(backgroundLayer2, .25);
+const Layer3 = new Layer(backgroundLayer3, 2);
+const Layer4 = new Layer(backgroundLayer4, 3);
+const Layer5 = new Layer(backgroundLayer5, 5);
+
+const gameObjects = [Layer1, Layer2, Layer3, Layer4, Layer5];
+
+function animate(){
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach(object => {
+         object.update();
+         object.draw();
+    })
+    requestAnimationFrame(animate)
+};
+
+animate();  
